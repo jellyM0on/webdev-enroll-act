3c42abeba2ea2753edadea3cf22b2889f183ee96
jellyM0on <ylana.ong@gmail.com>
2025-05-20 20:02:00 +0800

    update db config

--- db.php
+++ db.php
@@ -8,7 +8,6 @@ $password = "";
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $pdo;
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
